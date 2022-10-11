9431e00034d924aee63e288f69d61069b348c0cb
Sergio0002 <50211616+Sergio0002@users.noreply.github.com>
2022-10-11 14:30:34 +0200

    commit

--- ProjBuscaminas/js/Buscaminas.js
+++ ProjBuscaminas/js/Buscaminas.js
@@ -1,83 +1,75 @@
-let maxFilas = prompt("¿Cuántas filas quieres?");
-let maxColumnas = prompt("¿Cuántas columnas quieres?");
-let numMinas = prompt("¿Cuántas minas quieres introducir?");
+let maxFilas = prompt('¿Cuántas filas quieres?');
+let maxColumnas = prompt('¿Cuántas columnas quieres?');
+let numMinas = prompt('¿Cuántas minas quieres introducir?');
 
-document.write("<h1>Buscaminas</h1>");
 
-document.write("<table id='buscaminas'>");
+// Creamos el tablero en html
+
+document.write('<table>');
 
 for (let i = 0; i < maxFilas; i++) {
-
-    document.write("<tr>");
+    document.write('<tr>');
 
     for (let j = 0; j < maxColumnas; j++) {
-
-        document.write("<td></td>");
-
+        document.write('<td></td>');
     }
 
-    document.write("</tr>");
+    document.write('</tr>');
 }
-document.write("</table>");
+document.write('</table>');
+
 
 // Crear array bidimensional para guardar las minas
-
 let arrayTablero = [];
 let contadorMinas = 0;
+
+for (let fila = 0; fila < maxFilas; fila++) {
+    arrayTablero[fila] = new Array(maxColumnas);
+
+    for (let columna = 0; columna < maxColumnas; columna++) {
+        arrayTablero[fila][columna] = '';
+    }
+}
+
+
 let posFila;
 let posColumna;
 
-for (let fila = 0; fila < maxFilas; fila++) {
-
-    arrayTablero[fila] = [];
-
-    for (let columna = 0; columna < maxColumnas; columna++) {
-
-        arrayTablero[fila] = new Array(columna);
-    }
-}
 
 while (contadorMinas < numMinas) {
-
     posFila = Math.floor(Math.random() * maxFilas);
     posColumna = Math.floor(Math.random() * maxColumnas);
 
-    if (arrayTablero[posFila][posColumna] != "Mina") {
-        arrayTablero[posFila][posColumna] = "Mina";
+    if (arrayTablero[posFila][posColumna] != 'MINA') {
+        arrayTablero[posFila][posColumna] = 'MINA';
         contadorMinas++;
-    }
-}
+    };
+};
 
 
-let fila = 2;
-let columna = 1;
-let numMinasAlrededor = 0;
-
+let numMinasAlrededor;
+let columna = 0;
 
 for (let fila = 0; fila < maxFilas; fila++) {
-
     for (let columna = 0; columna < maxColumnas; columna++) {
+        numMinasAlrededor = 0;
+        if (arrayTablero[fila][columna] != "MINA") {
 
-        if (arrayTablero[fila][columna] != "Mina") {
-
-            for (let cFila = fila - 1; cFila <= fila + 1; cFila++) {
-
-                for (let cColumna = - 1; cColumna <= cColumna + 1; cColumna++) {
-
-                    if (cFila <= 0 || cColumna >= 0 || cFila > maxFilas || cColumna > maxColumnas) {
-
-                        if (arrayTablero[cFila][cColumna] == "Mina") {
-
+            for (let cFila = fila - 1; cFila < fila + 1; cFila++) {
+                for (let cColumna = columna - 1; cColumna < columna + 1; cColumna++) {
+                    if (cFila >= 0 && cFila < maxFilas && cColumna > 0 && cColumna < maxColumnas) {
+                        if (arrayTablero[cFila][cColumna] == 'MINA') {
                             numMinasAlrededor++;
                         }
-
                     }
                 }
             }
-
         }
 
     }
+
+    arrayTablero[fila][columna] = numMinasAlrededor;
 }
 
-arrayTablero[fila][columna] = numMinasAlrededor;
+
+console.log(arrayTablero);
---
+++ ProjBuscaminas/recursos/001.js
@@ -0,0 +1,7 @@
+var num1 = 89;
+var num2 = 1;
+
+var total = num1 + num2;
+
+console.log(total);
+
